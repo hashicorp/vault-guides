6271cac4b2b1ee7d6225d6694e4b3fa0af4b2b20
Lance Larsen <lance@hashicorp.com>
2018-06-09 17:14:21 -0700

    update build script

--- secrets/spring-cloud-vault/aws/packer/build.sh
+++ secrets/spring-cloud-vault/aws/packer/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FAIL=0
+
 #Get vars
 if [ -z ${PACKER_ENVIRONMENT} ]; then
   read -p $'\033[1;32mPlease enter your PACKER ENVIRONMENT: \033[0m' PACKER_ENVIRONMENT
@@ -22,6 +24,20 @@ else
   export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
 fi
 
-#Build it
-AWS_REGION="us-east-1" packer build -force springboot-ec2.json &
-AWS_REGION="us-east-1" packer build -force springboot-iam.json &
+#Start Jobs
+echo "Starting Packer builds..."
+export AWS_REGION=us-east-1
+packer build -force springboot-ec2.json &
+packer build -force springboot-iam.json &
+
+#Wait for completion
+for job in `jobs -p`; do
+  echo $job
+  wait $job || let "FAIL+=1"
+done
+
+if [ "$FAIL" == "0" ]; then
+  echo -e "\033[32m\033[1m[BUILD SUCCESFUL]\033[0m"
+else
+  echo -e "\033[31m\033[1m[BUILD ERROR]\033[0m"
+fi

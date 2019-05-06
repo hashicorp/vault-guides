acb385fd72a3cd24f0a70463dac1d628dcd47e2b
jeremiah <jeremiah@hashicorp.com>
2019-05-06 11:22:54 -0400

    changed - to _ for variable crypto_key

--- operations/gcp-kms-unseal/main.tf
+++ operations/gcp-kms-unseal/main.tf
@@ -82,7 +82,7 @@ output "vault_server_instance_id" {
 
 # Create a crypto key for the key ring
 # resource "google_kms_crypto_key" "crypto_key" {
-#   name            = "${var.crypto-key}"
+#   name            = "${var.crypto_key}"
 #   key_ring        = "${google_kms_key_ring.key_ring.self_link}"
 #   rotation_period = "100000s"
 # }

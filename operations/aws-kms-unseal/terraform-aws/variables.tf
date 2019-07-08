e92c713e1547f2843bbea533a9a65a34d2d565fb
Andy Davies <andydavies2020@gmail.com>
2019-07-08 20:48:51 +0100

    update aws-kms-unseal to terraform 0.12 syntax (#158)

--- operations/aws-kms-unseal/terraform-aws/variables.tf
+++ operations/aws-kms-unseal/terraform-aws/variables.tf
@@ -1,17 +1,17 @@
-variable aws_region {
+variable "aws_region" {
   default = "us-east-1"
 }
 
-variable aws_zone {
+variable "aws_zone" {
   default = "us-east-1a"
 }
 
-variable vault_url {
+variable "vault_url" {
   default = "https://releases.hashicorp.com/vault/1.1.2/vault_1.1.2_linux_amd64.zip"
 }
 
-variable vpc_cidr {
-  type        = "string"
+variable "vpc_cidr" {
+  type        = string
   description = "CIDR of the VPC"
   default     = "192.168.100.0/24"
 }

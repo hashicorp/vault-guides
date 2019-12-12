cd98cbcf5dbfc89522e6b6ce46270bd35831f45f
Lynn Frank <franklin.webber@gmail.com>
2019-12-12 12:30:58 -0600

    Removes some raft-aws outptut
    Initially I added a bunch of additional ways in which to reach the nodes
    in the cluster but found myself only interacting via SSH. Removes all
    the other things to make it easier to read and understand.

--- operations/raft-storage/aws/outputs.tf
+++ operations/raft-storage/aws/outputs.tf
@@ -6,9 +6,7 @@ output "endpoints" {
     - The root token creates a transit key that enables the other Vaults to auto-unseal.
     - Does not join the High-Availability (HA) cluster.
 
-    Local: VAULT_ADDR=http://${aws_instance.vault-transit.public_ip}:8200 vault
-    Web:   open http://${aws_instance.vault-transit.public_ip}:8200/ui/
-    SSH:   ssh -l ubuntu ${aws_instance.vault-transit.public_ip} -i ${var.key_name}.pem
+    $ ssh -l ubuntu ${aws_instance.vault-transit.public_ip} -i ${var.key_name}.pem
 
   vault_2 (${aws_instance.vault-server[0].public_ip})
     - Initialized and unsealed.
@@ -16,30 +14,24 @@ output "endpoints" {
     - K/V-V2 secret engine enabled and secret stored.
     - Leader of HA cluster
 
-    Local: VAULT_ADDR=http://${aws_instance.vault-server[0].public_ip}:8200 vault
-    Web:   open http://${aws_instance.vault-server[0].public_ip}:8200/ui/
-    SSH:   ssh -l ubuntu ${aws_instance.vault-server[0].public_ip} -i ${var.key_name}.pem
+    $ ssh -l ubuntu ${aws_instance.vault-server[0].public_ip} -i ${var.key_name}.pem
 
-    Root Token:
-        ssh -l ubuntu -i ${var.key_name}.pem ${aws_instance.vault-server[0].public_ip} 'cat /tmp/key.json | jq -r ".root_token"'
-    Recovery Key:
-        ssh -l ubuntu -i ${var.key_name}.pem ${aws_instance.vault-server[0].public_ip} 'cat /tmp/key.json | jq -r ".recovery_keys_b64[0]"'
+    # Root token:
+    $ ssh -l ubuntu ${aws_instance.vault-server[0].public_ip} -i ${var.key_name}.pem "cat ~/root_token"
+    # Recovery key:
+    $ ssh -l ubuntu ${aws_instance.vault-server[0].public_ip} -i ${var.key_name}.pem "cat ~/recovery_key"
 
   vault_3 (${aws_instance.vault-server[1].public_ip})
     - Started
-    - You will join it to the HA cluster.
+    - You will join it to cluster at http://${aws_instance.vault-server[0].public_ip}:8200
 
-    Local: VAULT_ADDR=http://${aws_instance.vault-server[1].public_ip}:8200 vault
-    Web:   open http://${aws_instance.vault-server[1].public_ip}:8200/ui/
-    SSH:   ssh -l ubuntu ${aws_instance.vault-server[1].public_ip} -i ${var.key_name}.pem
+    $ ssh -l ubuntu ${aws_instance.vault-server[1].public_ip} -i ${var.key_name}.pem
 
   vault_4 (${aws_instance.vault-server[2].public_ip})
     - Started
-    - You will join it to the HA cluster.
+    - You will join it to cluster at http://${aws_instance.vault-server[0].public_ip}:8200
 
-    Local: VAULT_ADDR=http://${aws_instance.vault-server[2].public_ip}:8200 vault
-    Web:   open http://${aws_instance.vault-server[2].public_ip}:8200/ui/
-    SSH:   ssh -l ubuntu ${aws_instance.vault-server[2].public_ip} -i ${var.key_name}.pem
+    $ ssh -l ubuntu ${aws_instance.vault-server[2].public_ip} -i ${var.key_name}.pem
 
 EOF
 }

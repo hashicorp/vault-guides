5bcf59d57644028e97ef1e106f59bce7e2931ab9
Yoko Hyakuna <yoko@hashicorp.com>
2020-01-14 15:32:19 -0800

    Fixed the rewrapping code to point 'city'

--- encryption/vault-transit-rewrap/DBHelper.cs
+++ encryption/vault-transit-rewrap/DBHelper.cs
@@ -20,9 +20,9 @@ namespace RewrapExample
                         "`user_name` VARCHAR(256) NOT NULL," +
                         "`first_name` VARCHAR(256) NULL, " +
                         "`last_name` VARCHAR(256) NULL, " +
-                        "`address` VARCHAR(256) NOT NULL, " +
                         "`city` VARCHAR(256) NOT NULL," +
                         "`state` VARCHAR(256) NOT NULL," +
+                        "`country` VARCHAR(256) NOT NULL," +
                         "`postcode` VARCHAR(256) NOT NULL," +
                         "`email` VARCHAR(256) NOT NULL," +
                         "PRIMARY KEY (user_id) " +
@@ -61,8 +61,8 @@ namespace RewrapExample
                 {
                     
                     string command = "INSERT INTO `user_data` " + 
-                    "(`user_name`, `first_name`, `last_name`, `address`, " +
-                    "`city`, `state`, `postcode`, `email`) " +
+                    "(`user_name`, `first_name`, `last_name`, " +
+                    "`city`, `state`, `country`, `postcode`, `email`) " +
                     $"VALUES (\"{r.Login.Username}\", \"{r.Name.First}\", \"{r.Name.Last}\", " +
                     $"\"{r.Location.City}\", \"{r.Location.State}\", \"{r.Location.Country}\", " +
                     $"\"{r.Location.Postcode}\", \"{r.Email}\");";
@@ -83,13 +83,12 @@ namespace RewrapExample
                 using (var cmd = db.Connection.CreateCommand())
                 {
                     string command = "UPDATE `user_data` " + 
-                    $"SET `address` = \"{r.Location.City}\", " +
+                    $"SET `city` = \"{r.Location.City}\", " +
                     $"`email` = \"{r.Email}\" " +
                     $"WHERE `user_id` = {r.Id.Value}";
                     
                     cmd.CommandText = command;
-                    Console.WriteLine("Command: " + command);
-
+                    
                     await cmd.ExecuteNonQueryAsync();
                 }
             }
@@ -106,10 +105,10 @@ namespace RewrapExample
                 using (var cmd = db.Connection.CreateCommand())
                 {
                     int count = 0;
-                    string command = "SELECT `user_id`, `email`, `address` " +
+                    string command = "SELECT `user_id`, `email`, `city` " +
                     "FROM `user_data` " + 
                     $"WHERE `email` NOT LIKE \"vault:v{keyVersion}:%\" " + 
-                    $"OR `address` NOT LIKE \"vault:v{keyVersion}:%\" ";
+                    $"OR `city` NOT LIKE \"vault:v{keyVersion}:%\" ";
                         
                     cmd.CommandText = command;
 
@@ -120,10 +119,10 @@ namespace RewrapExample
                         count++;
                         var user_id = reader.GetInt32(0);
                         var email = reader.GetString(1);
-                        var address = reader.GetString(2);
+                        var city = reader.GetString(2);
                         
                         RewrapExample.Location addr = new Location();
-                        addr.City = address;
+                        addr.City = city;
                         RewrapExample.Id id = new Id();
                         id.Value = user_id.ToString();
 

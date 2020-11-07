73b56ffdd63e56f9229c1f1c6034f7e2a98e8b69
Yoko Hyakuna <yoko@hashicorp.com>
2020-11-07 12:02:10 -0800

    Adding nested namespaces

--- operations/codify-mgmt/enterprise/policies.tf
+++ operations/codify-mgmt/enterprise/policies.tf
@@ -29,3 +29,35 @@ resource "vault_policy" "fpe_client_policy" {
   name   = "fpe-client"
   policy = file("policies/fpe-client-policy.hcl")
 }
+
+# Create admin policy in the education namespace
+resource "vault_policy" "admin_policy_education" {
+  provider = vault.education
+  depends_on = [vault_namespace.education]
+  name   = "admins"
+  policy = file("policies/admin-policy.hcl")
+}
+
+# Create admin policy in the 'education/training' namespace
+resource "vault_policy" "admin_policy_training" {
+  provider = vault.training
+  depends_on = [vault_namespace.training]
+  name   = "admins"
+  policy = file("policies/admin-policy.hcl")
+}
+
+# Create admin policy in the 'education/training/vault_cloud' namespace
+resource "vault_policy" "admin_policy_vault_cloud" {
+  provider = vault.vault_cloud
+  depends_on = [vault_namespace.vault_cloud]
+  name   = "admins"
+  policy = file("policies/admin-policy.hcl")
+}
+
+# Create admin policy in the 'education/training/boundary' namespace
+resource "vault_policy" "admin_policy_boundary" {
+  provider = vault.boundary
+  depends_on = [vault_namespace.boundary]
+  name   = "admins"
+  policy = file("policies/admin-policy.hcl")
+}

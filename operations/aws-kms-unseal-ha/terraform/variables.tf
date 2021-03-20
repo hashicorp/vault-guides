06d16087d0f48425737b8e6b4fbe790830532cec
Kelly Hair <kelly.hair@mayadata.io>
2021-03-20 13:04:02 -0400

    Cleaned up variables.tf

--- operations/aws-kms-unseal-ha/terraform/variables.tf
+++ operations/aws-kms-unseal-ha/terraform/variables.tf
@@ -6,17 +6,6 @@ variable aws_zone {
   default = "us-east-1a"
 }
 
-# variable "subnets_cidr" {
-#   type = list
-#   default = [
-#     "10.200.1.0/24", 
-#     "10.200.2.0/24", 
-#     "10.200.3.0/24",
-#     "10.200.4.0/24",
-#     "10.200.5.0/24"
-#     ]
-# }
-
 variable vpc_cidr {
   description = "CIDR of the VPC"
   default     = "10.200.0.0/16"

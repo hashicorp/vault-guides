0371eb1fb0a782c9028d19491fda600496f7c194
Brian Shumate <brian@hashicorp.com>
2021-04-05 15:15:13 -0400

    Configuration cleanup

--- cloud/terraform-hcp-vault/hcp-vault-vpc/vault.tf
+++ cloud/terraform-hcp-vault/hcp-vault-vpc/vault.tf
@@ -4,21 +4,7 @@ resource "hcp_hvn" "learn_hcp_vault_hvn" {
   region         = var.region
 }
 
-resource "hcp_hvn" "learn_hcp_vault_hvn" {
-  hvn_id         = var.hvn_id
-  cloud_provider = var.cloud_provider
-  region         = var.region
-}
-
 resource "hcp_vault_cluster" "learn_hcp_vault" {
   hvn_id     = hcp_hvn.learn_hcp_vault_hvn.hvn_id
   cluster_id = "hcp-tf-example-vault-cluster"
 }
-
-# data "hcp_vault_cluster_admin_token" "learn_hcp_vault_admin_token" {
-#   cluster_id = hcp_vault_cluster.example_vault_cluster.cluster_id
-# }
-#
-# output "initial_root_token" {
-#   value = learn_hcp_vault_admin_token
-# }

75c34822445d9b896fa7ab54aef5e62a1a8ff6da
Brian Shumate <brian@hashicorp.com>
2021-04-05 13:21:54 -0400

    Add some example Terraform for HCP Vault

---
+++ cloud/terraform-hcp-vault/hcp-vault-base/variables.tf
@@ -0,0 +1,23 @@
+variable "hvn_id" {
+  description = "The ID of the HCP HVN."
+  type        = string
+  default     = "learn-hcp-vault-hvn"
+}
+
+variable "cluster_id" {
+  description = "The ID of the HCP Vault cluster."
+  type        = string
+  default     = "learn-hcp-vault"
+}
+
+variable "region" {
+  description = "The region of the HCP HVN and Vault cluster."
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "cloud_provider" {
+  description = "The cloud provider of the HCP HVN and Vault cluster."
+  type        = string
+  default     = "aws"
+}
---
+++ cloud/terraform-hcp-vault/hcp-vault-vpc/vpc-peering.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "peer" {
+  cidr_block = "172.31.0.0/16"
+}
+
+data "aws_arn" "peer" {
+  arn = aws_vpc.peer.arn
+}
+
+resource "hcp_aws_network_peering" "peer" {
+  hvn_id              = hcp_hvn.learn_hcp_vault_hvn.hvn_id
+  peer_vpc_id         = aws_vpc.peer.id
+  peer_account_id     = aws_vpc.peer.owner_id
+  peer_vpc_region     = data.aws_arn.peer.region
+  peer_vpc_cidr_block = aws_vpc.peer.cidr_block
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
+  auto_accept               = true
+}

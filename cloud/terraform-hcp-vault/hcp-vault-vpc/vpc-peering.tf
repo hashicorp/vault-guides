345cc467f57cb48fdb7a960ed76ca5646519118d
Brenna Hewer-Darroch <21015366+bcmdarroch@users.noreply.github.com>
2021-07-06 21:56:20 -0700

    add HVN route example (#341)
    * add HVN route example
    
    * Add peering_id
    
    Co-authored-by: Yoko Hyakuna <yoko@hashicorp.com>

--- cloud/terraform-hcp-vault/hcp-vault-vpc/vpc-peering.tf
+++ cloud/terraform-hcp-vault/hcp-vault-vpc/vpc-peering.tf
@@ -12,10 +12,17 @@ data "aws_arn" "peer" {
 
 resource "hcp_aws_network_peering" "peer" {
   hvn_id              = hcp_hvn.learn_hcp_vault_hvn.hvn_id
+  peering_id          = var.peering_id
   peer_vpc_id         = aws_vpc.peer.id
   peer_account_id     = aws_vpc.peer.owner_id
   peer_vpc_region     = data.aws_arn.peer.region
-  peer_vpc_cidr_block = aws_vpc.peer.cidr_block
+}
+
+resource "hcp_hvn_route" "peer_route" {
+  hvn_link         = hcp_hvn.learn_hcp_vault_hvn.self_link
+  hvn_route_id     = var.route_id
+  destination_cidr = aws_vpc.peer.cidr_block
+  target_link      = hcp_aws_network_peering.peer.self_link
 }
 
 resource "aws_vpc_peering_connection_accepter" "peer" {

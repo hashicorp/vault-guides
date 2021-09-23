049bfa4d472651128016dc7e11b0856495b71c93
Yoko Hyakuna <yoko@hashicorp.com>
2021-09-22 20:45:40 -0700

    Remove comment

--- cloud/terraform-hcp-vault/hcp-vault-base/variables.tf
+++ cloud/terraform-hcp-vault/hcp-vault-base/variables.tf
@@ -23,7 +23,6 @@ variable "cloud_provider" {
 }
 
 variable "tier" {
-  # Available values are: dev, standard_small, standard_medium, standard_large, starter_small
   description = "Tier of the HCP Vault cluster. Valid options for tiers."
   type        = string
   default     = "dev"

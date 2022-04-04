ed8bc91132c89f8ddb9996b7d8209c4977f2e85c
Jan Prinsloo <jan.prinsloo@hashicorp.com>
2022-04-04 09:29:37 +0200

    Upgrade to the latest terraform & azure providers
    This makes the guide work in the latest terraform:
    
    Terraform v1.1.7
    + provider.azuread v2.19.1
    + provider.azurerm v3.0.2
    + provider.random v3.1.2
    + provider.template v2.2.0
    
    Key Permissions are now case sensitive:
    + "Get",
    + "WrapKey",
    + "UnwrapKey",
    + "Get",
    + "List",
    + "Create",
    + "Delete",
    + "Update",

--- operations/azure-keyvault-unseal/main.tf
+++ operations/azure-keyvault-unseal/main.tf
@@ -1,11 +1,11 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.1.7"
   required_providers {
-    template = "~> 2.1.2"
-    random = "~> 2.3.0"
-    azurerm = "~> 2.20.0"
-    azuread = "~> 0.11.0"
+    template = "~> 2.2.0"
+    random = "~> 3.1.2"
+    azurerm = "~> 3.0.2"
+    azuread = "~> 2.19.1"
   }
 }
 
@@ -57,9 +57,9 @@ resource "azurerm_key_vault" "vault" {
     object_id = data.azuread_service_principal.vault.object_id
 
     key_permissions = [
-      "get",
-      "wrapKey",
-      "unwrapKey",
+      "Get",
+      "WrapKey",
+      "UnwrapKey",
     ]
   }
 
@@ -69,11 +69,11 @@ resource "azurerm_key_vault" "vault" {
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
-      "get",
-      "list",
-      "create",
-      "delete",
-      "update",
+      "Get",
+      "List",
+      "Create",
+      "Delete",
+      "Update",
     ]
   }
 
